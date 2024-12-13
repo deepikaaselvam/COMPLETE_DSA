641dca40db0a092a285e226274e2bf9b6af67b2f
Deepika.S <145779798+deepikaaselvam@users.noreply.github.com>
2024-12-13 09:33:46 +0530

    Adjusting the tab spaces

--- Hackerrank.java
+++ Hackerrank.java
@@ -13,15 +13,14 @@ public class Hackerrank {
             rightDiagonal+=arr.get(i).get(n-i-1);
                 
         }
-        
         ans=LeftDiagonal-rightDiagonal;
         return Math.abs(ans);
         }
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> matrix=new ArrayList<>();
-       matrix.add(new ArrayList<>(Arrays.asList(1, 2, 7)));
-        matrix.add(new ArrayList<>(Arrays.asList(4, 5, 6)));
-        matrix.add(new ArrayList<>(Arrays.asList(7, 8, 9)));
+            matrix.add(new ArrayList<>(Arrays.asList(1, 2, 7)));
+            matrix.add(new ArrayList<>(Arrays.asList(4, 5, 6)));
+            matrix.add(new ArrayList<>(Arrays.asList(7, 8, 9)));
         System.out.println(diagonalDifference(matrix));
     }
     
