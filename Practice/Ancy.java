e43fa6a3c7f304f0976f522e9b5b5685f8855101
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-12-26 21:32:39 +0530

    Some of the Basic codes of typeCasting

---
+++ Practice/Ancy.java
@@ -0,0 +1,16 @@
+package Practice;
+import java.util.Scanner;
+public class Ancy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch =sc.next().charAt(0);
+        System.out.println(((int)(ch)));
+        float f=(int)0.9;
+        System.out.println(f);
+        sc.close();
+
+
+        
+    }
+    
+}
