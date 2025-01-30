1a17f0c8c8e0c9d033b478472c8304b2ce60ceea
deepikaaselvam <deepikaselvam.16@gmail.com>
2025-01-30 22:46:03 +0530

    ASCII Character

---
+++ Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    public static void main(String[] args) {
+      int cnt=0;
+      while(cnt<26)
+      {
+        char ch= (char) ('a' + cnt);
+        System.out.print(ch+" ");
+        cnt++;
+      }
+ 
+    }
+    
+}
