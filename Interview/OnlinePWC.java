342427c6103576c981f89fc32134e07463dad23a
deepikaaselvam <deepikaselvam.16@gmail.com>
2025-02-01 00:28:38 +0530

    Today Online Assessment for Software Developer by PWC,Bangalore,India

---
+++ Interview/OnlinePWC.java
@@ -0,0 +1,31 @@
+package Interview;
+public class OnlinePWC{
+    public static char missingCharcater(char[] array)
+    {
+        if(array.length==0)
+        {
+            return ' ';
+        }
+       char[]extraCharacterArray= new char[array.length+1];
+        for(int i=0;i<extraCharacterArray.length;i++)
+        {
+            extraCharacterArray[i]=(char)('a'+i);
+        }
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]!=extraCharacterArray[i])
+            {
+                return extraCharacterArray[i];
+            }
+        }
+
+        return extraCharacterArray[extraCharacterArray.length-1];
+
+    }
+
+    public static void main(String[] args) {
+        char[]array={};//{'a','b','d','e','f'};//{'a','b','c'};//{'a','b','c','d','e','g','h','i','j','k'};//{'a','b','d','e','f'};
+        System.out.println(missingCharcater(array));
+        
+    }
+}
