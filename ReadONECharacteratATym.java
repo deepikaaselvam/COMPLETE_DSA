1d727c71e8342faccdde11ed2aff23b3c278239f
deepikaaselvam <deepikaselvam.16@gmail.com>
2025-02-16 23:37:39 +0530

    One CHarcater at a tym

---
+++ ReadONECharacteratATym.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+public class ReadONECharacteratATym {
+
+    public static void main(String[] args)throws IOException {
+        // Scanner kbd = new Scanner(System.in);
+        int n= System.in.read(); //ReadsOnly One charcater at the Tym
+        System.out.println(n-48); 
+        
+    }
+}
+  
+    
+
