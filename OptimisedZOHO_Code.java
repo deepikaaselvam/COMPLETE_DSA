1f4eb4513097dd80bb2d37fe51db2cfb58d5515d
deepikaaselvam <deepikaselvam.16@gmail.com>
2025-02-16 22:52:21 +0530

    Remove Charcater from the String to Provide the Maximum Number of the String

---
+++ OptimisedZOHO_Code.java
@@ -0,0 +1,25 @@
+// import java.lang.Stringbuilder;
+public class OptimisedZOHO_Code {
+    public static void main(String[] args) {
+        String number ="98919";
+        String digits ="9";
+        StringBuilder sb = new StringBuilder(number);
+        int maximum = Integer.MIN_VALUE;
+        for(int i=0;i<sb.length();i++)
+        {
+            if(sb.charAt(i)==digits.charAt(0))
+            {
+                char removed = sb.charAt(i);
+                sb.deleteCharAt(i);
+                int nukber = Integer.parseInt(sb.toString());
+                maximum = Math.max(maximum,nukber);
+
+                //insert the removed charcater again into the stringBuilder
+                sb.insert(i,removed);
+
+            }
+        }
+        System.out.println(maximum);
+    }
+    
+}
